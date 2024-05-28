515eebc8f989524996c0dae9fa95da9ad3e166c1
Twan code his life <nddt1810@gmail.com>
2024-05-29 02:53:58 +0700

    Update model.py

--- app/model.py
+++ app/model.py
@@ -6,42 +6,45 @@ import re
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoConfig
 import numpy as np
 from scipy.special import softmax
+import logging
+from transformers import logging as transformers_logging
+transformers_logging.set_verbosity_error()
 
-# Download NLTK data if not already downloaded
+# Tải dữ liệu NLTK nếu chưa được tải xuống
 nltk.download('punkt')
 nltk.download('vader_lexicon')
 nltk.download('stopwords')
 
-# Initialize VADER for sentiment analysis
+# Khởi tạo VADER cho phân tích cảm xúc
 sa = SentimentIntensityAnalyzer()
 
-# Load RoBERTa tokenizer, configuration and model
+# Tải tokenizer, cấu hình và mô hình RoBERTa
 MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 config = AutoConfig.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
 def analyze_sentiment(text):
-    # Remove digits
+    # Xóa các chữ số và ký tự không phải là từ hoặc khoảng trắng
     text_final = re.sub(r'\d+', '', text)
     text_final = re.sub(r'[^\w\s]', '', text_final)
-    # Tokenize the text
+    # Tokenize văn bản
     words = word_tokenize(text_final)
 
-    # Remove stopwords
+    # Xóa các stopwords
     stop_words = set(stopwords.words('english'))
     processed_doc = ' '.join([word for word in words if word.lower() not in stop_words])
 
-    # Sentiment analysis using VADER
+    # Phân tích cảm xúc sử dụng VADER
     dd_vader = sa.polarity_scores(processed_doc)
 
-    # Sentiment analysis using RoBERTa
+    # Phân tích cảm xúc sử dụng RoBERTa
     encoded_input = tokenizer(text, return_tensors='pt')
     output = model(**encoded_input)
     scores_roberta = output[0][0].detach().numpy()
     scores_roberta = softmax(scores_roberta)
 
-    # Determine the sentiment label and score from RoBERTa
+    # Xác định nhãn cảm xúc và điểm số từ RoBERTa
     label_roberta = config.id2label[np.argmax(scores_roberta)]
     score_roberta = {
         'positive': scores_roberta[2],
@@ -49,7 +52,7 @@ def analyze_sentiment(text):
         'negative': scores_roberta[0]  
     }
 
-    # Determine the maximum VADER score
+    # Xác định điểm số VADER cao nhất
     max_vader_score = max(dd_vader['pos'], dd_vader['neu'], dd_vader['neg'])
 
     return {
